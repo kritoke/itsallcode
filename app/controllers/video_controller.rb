294de1a7c0fa1e53eddfe4985e7562f98bb63fde
Kritoke <matthew.rhone@outlook.com>
2017-04-04 07:28:57 -0500

    Make routes accept trailing slash.

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -2,12 +2,12 @@ class VideoController < ApplicationController
   include Slugifiable::InstanceMethods
   extend Slugifiable::ClassMethods
 
-  get '/videos' do
-
+  get '/videos/?' do
+    @videos = Video.all
     erb :'videos/index'
   end
 
-  get '/videos/new' do
+  get '/videos/new/?' do
     erb :'videos/new'
   end
 
@@ -16,12 +16,12 @@ class VideoController < ApplicationController
     redirect "/videos/#{video.language}/#{video.id}"
   end
 
-  get '/videos/:language' do
+  get '/videos/:language/?' do
     @videos = Video.where(language: params[:language]) # make variable contain only :language
     erb :'videos/languages'
   end
 
-  get '/videos/:language/:id' do
+  get '/videos/:language/:id/?' do
     @video = Video.find_by(id: params[:id])
     erb :'videos/show'
   end
