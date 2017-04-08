18dc879212ea1433f24217eb8e273f5541203892
Kritoke <kritoke@Kritokes-MacBook-Air.local>
2017-04-08 15:51:20 -0500

    Added edit ability, tweak video controller, start partially working on npm script setup.

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -22,9 +22,34 @@ class VideoController < ApplicationController
     erb :'videos/languages'
   end
 
+  get '/:language/:id/edit' do
+    if logged_in?
+      @video = Video.find_by(id: params[:id])
+      erb :'videos/edit'
+    else
+      redirect '/login'
+    end
+  end
+
   get '/:language/:id/?' do
     @video = Video.find_by(id: params[:id])
     erb :'videos/show'
+  end
+
+  post '/videos/:id' do
+    video = Video.find_by(id: params[:id])
+    if logged_in?
+      if !params[:language].empty? && !params[:url].empty?
+        url = Yt::URL.new params[:url]
+        yt_video = Yt::Video.new id: url.id
+        video = Video.update(title: yt_video.title, description: yt_video.description, view_count: yt_video.view_count, thumbnail_url: yt_video.thumbnail_url, published: yt_video.published_at, yt_id: yt_video.id, language: params[:language].downcase)
+        redirect "/#{params[:language]}/#{params[:id]}"
+      else
+        redirect "/#{params[:language]}/#{params[:id]}/edit"
+      end
+    else
+      redirect "/login"
+    end
   end
 
 end
